dbe084ca062edaf7ebfb819916c737dc9b16d25d
BB9Kairo <85286527+calebjubal@users.noreply.github.com>
2024-03-01 10:27:08 +0530

    Add files via upload

---
+++ userAuthForm.page.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import InputBox from '../components/input.component';
+import googleIcon from "../imgs/google.png"
+import { Link } from 'react-router-dom';
+
+const UserAuthForm = ({ type }) => {
+  return (
+    <section className="h-cover flex items-top justify-center ">
+        <form className="w-[80%] max-w-[400px]">
+            <h1 className="text-4xl font-gelasio capitalise text-center mb-24">
+                {type == "sign-in" ? "Welcome back" : "Join us Today"}
+            </h1>
+
+            {
+                type != "sign-in" ?
+                <InputBox 
+                    name= "fullname"
+                    type="text"
+                    placeholder="Full Name"
+                    icon="fi-rr-user"    
+                />
+                : ""
+            }
+
+            <InputBox 
+                name= "email"
+                type="email"
+                placeholder="Email"
+                icon="fi-rr-envelope"    
+            />
+
+            <InputBox 
+                name= "password"
+                type="password"
+                placeholder="Password"
+                icon="fi-rr-key"    
+            />
+
+            <button
+                className="btn-dark center mt-14"
+                type="submit"
+            >
+                { type.replace("-"," ") }
+            </button>
+
+            <div className="relative w-full flex items-cenetr gap-2 my-10 opacity-10 uppercase text-black font-bold">
+                <hr className="w-1/2 border-black"/>
+                <p>or</p>
+                <hr className="w-1/2 border-black"/>
+            </div>
+
+            <button className="btn-dark flex items-top justify-center gap-4 w-[90%] center">
+                <img src={googleIcon} className="w-5"/>
+                continue with google
+            </button>
+
+            {
+                type == "sign-in" ?
+                <p className="mt-6 text-dark-grey text-xl text-center">
+                     Don't have an account ? 
+                    <Link to="/signup" className="underline text-black text-xl ml-1">
+                        Join us Today 
+                    </Link>
+                </p> 
+                :
+                <p className="mt-6 text-dark-grey text-xl text-center">
+                    Already a member ? 
+                    <Link to="/signup" className="underline text-black text-xl ml-1">
+                        Sign in here.
+                    </Link>
+                </p> 
+            }
+        </form>
+    </section>
+  )
+}
+
+export default UserAuthForm;
